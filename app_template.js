e9d87630d60f0ce868c7c519b27782272fdbf8f3
kasidej02 <kasidej.kammool@gmail.com>
2021-01-12 13:50:11 +0700

    1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,6 @@
 //your code here!
+function calculate(){
+    var num = document.getElementById("button-compute").value;
+    var para = document.createAttribute('div');
+    para.appendChild(num);
+}
