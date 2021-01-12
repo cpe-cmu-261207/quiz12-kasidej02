c5e0fdc6a30f5ec3bbfb1a380e0ad2a9627dd9ee
kasidej02 <kasidej.kammool@gmail.com>
2021-01-12 15:34:40 +0700

    1

--- app_template.js
+++ app_template.js
@@ -11,17 +11,24 @@ function createTable(){
     document.getElementById('show1').innerText = 'Created By Kasidej Kammool 620610776'
     let row = parseInt(document.getElementById('input-row').value);
     let col = parseInt(document.getElementById('input-col').value);
-    const table1 = document.querySelector('table');
-    const tr = document.createElement('tr');
-    const td = document.createElement('td');
-    var i;
-    var j;
-    for(i=0;i<row;i++){
-        for(j=0;j<col;j++){
-            table1.appendChild(td);
-            table1.innerHTML=1;
-        }
-        table1.appendChild(tr);
-        table1.innerHTML=1;
-    }
-}
+    var num=0;
+    var cnt=0;
+    for(var r=0;r<row;r++)
+    {
+        num++;
+     var x=document.getElementById('table').insertRow(r);
+     for(var c=0;c<col;c++)  
+      {
+        cnt++;
+       var y=  x.insertCell(c);
+       y.innerHTML=cnt;
+       if(num%2!=0 && cnt%2!=0){
+        y.innerHTML='*';
+       }
+       if(num%2==0 && cnt%2==0){
+        y.innerHTML='*';
+       }
+       
+      }
+     }
+  }
