64d5048b92f61d9f985bb2e8471c8468a4351d56
kasidej02 <kasidej.kammool@gmail.com>
2021-01-12 15:06:28 +0700

    a

--- app_template.js
+++ app_template.js
@@ -1,7 +1,27 @@
 //your code here!
 let myStu = 620610776;
+
 function calculate(){
     var num = document.getElementById('input-number').value;
     let re = myStu+parseInt(num);
     document.getElementById('show').innerText = re;
+}
+
+function createTable(){
+    document.getElementById('show1').innerText = 'Created By Kasidej Kammool 620610776'
+    let row = parseInt(document.getElementById('input-row').value);
+    let col = parseInt(document.getElementById('input-col').value);
+    const table1 = document.querySelector('table');
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    var i;
+    var j;
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++){
+            table1.appendChild(td);
+            table1.innerHTML=1;
+        }
+        table1.appendChild(tr);
+        table1.innerHTML=1;
+    }
 }
